46c1153becad9f103265b6227c82fb0abd43d341
charlotte henriksson <charlotte.henriksson@iths.se>
2023-09-14 13:30:15 +0200

    Fixat utskrifter enligt AppTest enhanced switch, ändrat i minmaxmedel metoden, ändrat i sortera metoden, små fix i metoderna och kommenterat

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.print("Hello There!");
+
         Locale.setDefault(Locale.forLanguageTag("sv-SE"));
         Scanner input = new Scanner(System.in);
 
